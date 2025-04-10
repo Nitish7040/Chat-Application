8f10f797da483f169c358ca8d8e3a047c11d6bb2
Nitish pandey <Nitish7040@users.noreply.github.com>
2025-04-11 03:10:49 +0530

    adding the right-side-bar of chat app ui

--- src/Components/RightSideBar/RightSideBar.jsx
+++ src/Components/RightSideBar/RightSideBar.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
 import './RightSideBar.css'
+import assets from '../../assets/assets'
 
 const RightSideBar = () => {
   return (
-    <div>RightSideBar</div>
+    <div className='rs'>
+      <div className="rs-profile">
+        <img src={assets.profile_img} alt="" />
+        <h3>Priyanshu kashyap 
+          <img className='dot' src={assets.green_dot} alt="" />
+        </h3>
+        <p>Hey, There i am using the chat app devlop by Nitish..</p>
+      </div>
+      <hr />
+      <div className="rs-media">
+        <p>Media</p>
+        <div>
+          <img src={assets.pic1} alt="" />
+          <img src={assets.pic2} alt="" />
+          <img src={assets.pic3} alt="" />
+          <img src={assets.pic4} alt="" />
+          <img src={assets.pic1} alt="" />
+          <img src={assets.pic2} alt="" />
+        </div>
+      </div>
+      <button>Logout</button>
+    </div>
   )
 }
 
